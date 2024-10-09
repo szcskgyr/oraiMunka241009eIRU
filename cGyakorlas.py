ab2ef6b79bc34a5f36b0c2dc41546774e0d5e360
szcskgyr <szucsik.gyorgy@diak.szamalk-szalezi.hu>
2024-10-09 18:47:43 +0200

    ciklusok2

--- cGyakorlas.py
+++ cGyakorlas.py
@@ -1,3 +1,5 @@
+import math
+
 def harom():
     # 1. megoldás
     for i in range(0,21,1):
@@ -26,6 +28,10 @@ def ot():
 
 def beolvas():
     szam = int(input("Kérem adjon meg egy egész számot!"))
+    return szam
+
+def beolvas2():
+    szam = float(input("Kérem adjon meg egy számot!"))
     return szam
 
 def hat():
@@ -57,5 +63,74 @@ def het():
     else:
         for i in range(0, szorzat+1,1):
             print(i, end=" ")
+
+
+def nyolc():
+    # 8. Írd meg a fenti feladatot while és for ciklussal is!
+    szam1 = beolvas()
+    szam2 = beolvas()
+    szorzat = szam1 * szam2
+
+    i = 0
+
+    while i > szorzat-1:
+        print(i, end=" ")
+        i -= 1
+
+    i = 0
+
+    while i < szorzat+1:
+        print(i, end=" ")
+        i += 1
+
+
 def kilenc():
-    pass
+    # 9.	Írasd ki az első 7 pozitív egész számot vesszővel elválasztva!
+
+
+    for i in range(0,8,1):
+        print(i, end=",")
+        i += 1
+def kilencA():
+    # a.	úgy, hogy a végén ne legyen vessző!
+
+    # első megoldás
+    for i in range(0,7,1):
+        print(i, end=",")
+        i += 1
+    print("7")
+
+    # második megoldás
+    """
+    print("0", end="")
+    for i in range(1,8,1):
+        print(","+str(i), end="")
+        i += 1
+    """
+
+def tizenegy():
+    # 11. Írasd ki 2 bekért szám (x és y) alapján, hogy mennyi 3x+y^2!
+
+    x = beolvas2()
+    y = beolvas2()
+
+    eredmeny = 3*x + y**2
+    eredmeny1 = 3*x + math.pow(y,2)
+    eredmeny2 = 3*x + pow(y, 2)
+    eredmeny3 = 3*x + y*y
+
+    print("3x" + str(x) + " + " + str(y) + "^2" + " = " + str(eredmeny))
+
+def tizenketto():
+    # 12. Számold meg 2 bekért szám közötti páros számokat! (pl. hány db páros szám van 4 és 31 között?)
+
+    x = beolvas2()
+    y = beolvas2()
+    db = 0
+    for szam in range(math.ceil(x), round(y)+1, 1):
+        # print(szam,end=" ")
+        if szam%2 == 0:
+            # páros
+            db += 1
+    print("A páros számok száma: "+str(db)+".")
+
